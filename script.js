f91e2f7948552c1ed4c1308f6d3e01c86bb4cff3
Mario Müller <mariomuller@MacBook-Pro.net>
2023-09-14 22:40:27 +0200

    add a winner line

--- script.js
+++ script.js
@@ -16,6 +16,54 @@ function init() {
     render();
 }
 
+
+function checkWin() {
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontal
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Vertikal
+        [0, 4, 8], [2, 4, 6] // Diagonal
+    ];
+
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (fields[a] && fields[a] === fields[b] && fields[a] === fields[c]) {
+            return pattern;
+        }
+    }
+    return null;
+}
+
+function drawWinLine(pattern) {
+    if (pattern) {
+        const [a, b, c] = pattern;
+        const container = document.getElementById('board');
+        const cellA = document.getElementById(`cell-${a}`);
+        const cellB = document.getElementById(`cell-${b}`);
+        const cellC = document.getElementById(`cell-${c}`);
+
+        const svgns = "http://www.w3.org/2000/svg";
+        const svg = document.createElementNS(svgns, 'svg');
+        const line = document.createElementNS(svgns, 'line');
+
+        svg.setAttribute('width', '100%');
+        svg.setAttribute('height', '100%');
+        svg.setAttribute('xmlns', svgns);
+        svg.style.position = 'absolute'; // Füge den position-Stil hinzu
+
+        line.setAttribute('x1', cellA.offsetLeft + cellA.clientWidth / 2);
+        line.setAttribute('y1', cellA.offsetTop + cellA.clientHeight / 2);
+        line.setAttribute('x2', cellC.offsetLeft + cellC.clientWidth / 2);
+        line.setAttribute('y2', cellC.offsetTop + cellC.clientHeight / 2);
+        line.setAttribute('stroke', 'white');
+        line.setAttribute('stroke-width', '5');
+
+        svg.appendChild(line);
+        // Füge das SVG-Element vor dem ersten Kind von "board" ein
+        container.insertBefore(svg, container.firstChild);
+    }
+}
+
+
 function handleCellClick(index) {
     // Überprüfen, ob das Feld bereits belegt ist
     if (fields[index] === null) {
@@ -27,10 +75,17 @@ function handleCellClick(index) {
         // Entfernen des onclick-Attributs, um weitere Klicks zu verhindern
         cell.removeAttribute('onclick');
 
-        // Wechseln zum nächsten Spieler
-        currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
+        // Überprüfen, ob das Spiel vorbei ist
+        const winPattern = checkWin();
+        if (winPattern) {
+            drawWinLine(winPattern);
+        } else {
+            // Wechseln zum nächsten Spieler
+            currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
+        }
     }
 }
+
 
 function render() {
     const container = document.getElementById('content');
@@ -96,7 +151,4 @@ function generateCrossSVGCode() {
 
     return svgCode;
 }
-
-
-
 
