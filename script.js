20b8fd5684da7983c4729fc7f34bbf8424fbcd80
Mario Müller <mariomuller@macbook-pro.speedport.ip>
2023-09-16 10:25:37 +0200

    fix change to Developer Akkademie code step 2

--- script.js
+++ script.js
@@ -24,17 +24,98 @@ function init() {
 }
 
 
-// function checkWin() {
-//     for (const pattern of winPatterns) {
-//         const [a, b, c] = pattern;
-//         if (fields[a] && fields[a] === fields[b] && fields[a] === fields[c]) {
-//             return pattern;
-//         }
-//     }
-//     return null;
-// }
+function handleClick(cell, index) {
+    if (fields[index] === null) {
+        fields[index] = currentPlayer;
+        cell.innerHTML = currentPlayer === 'circle' ? generateCircleSVG() : generateCrossSVG();
+        cell.onclick = null;
+        currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
+        
+        if (isGameFinished()) {
+            const winCombination = getWinningCombination();
+            drawWinningLine(winCombination);
+        }
+    }
+}
 
-function drawWinLine(pattern) {
+
+function render() {
+    const contentDiv = document.getElementById('content');
+    // Generate table HTML
+    let tableHtml = '<table id="board">';
+    for (let i = 0; i < 3; i++) {
+        tableHtml += '<tr>';
+        for (let j = 0; j < 3; j++) {
+            const index = i * 3 + j;
+            let symbol = '';
+            if (fields[index] === 'circle') {
+                symbol = generateCircleSVG();
+            } else if (fields[index] === 'cross') {
+                symbol = generateCrossSVG();
+            }
+            tableHtml += `<td id="cell-${index}" onclick="handleClick(this, ${index})">${symbol}</td>`;
+        }
+        tableHtml += '</tr>';
+    }
+    tableHtml += '</table>';
+    // Set table HTML to contentDiv
+    contentDiv.innerHTML = tableHtml;
+}
+
+
+function isGameFinished() {
+    return fields.every((field) => field !== null) || getWinningCombination() !== null;
+}
+
+
+function getWinningCombination() {
+    for (let i = 0; i < winPatterns.length; i++) {
+        const [a, b, c] = winPatterns[i];
+        if (fields[a] === fields[b] && fields[b] === fields[c] && fields[a] !== null) {
+            return winPatterns[i];
+        }
+    }
+    return null;
+}
+
+
+function generateCircleSVG() {
+    const width = 70;
+    const height = 70;
+    const color = "#00B0EF";
+
+    const svgCode = `
+        <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="${width / 2}" cy="${height / 2}" r="${Math.min(width, height) / 2 - 5}" fill="none" stroke="${color}" stroke-width="10">
+                <animate attributeName="r" begin="0s" dur="200ms" values="0;${Math.min(width, height) / 2 - 5}" calcMode="linear" repeatCount="1" />
+            </circle>
+        </svg>
+    `;
+
+    return svgCode;
+}
+
+function generateCrossSVG() {
+    const width = 50;
+    const height = 50;
+    const color = "#FFC000";
+
+    const svgCode = `
+        <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
+            <line x1="0" y1="0" x2="${width}" y2="${height}" stroke="${color}" stroke-width="10" stroke-dasharray="${Math.sqrt(width * width + height * height)} 0" stroke-linecap="round">
+                <animate attributeName="stroke-dasharray" begin="0s" dur="200ms" values="0 ${Math.sqrt(width * width + height * height)};${Math.sqrt(width * width + height * height)} ${Math.sqrt(width * width + height * height)}" calcMode="linear" repeatCount="1" />
+            </line>
+            <line x1="0" y1="${height}" x2="${width}" y2="0" stroke="${color}" stroke-width="10" stroke-dasharray="${Math.sqrt(width * width + height * height)} 0" stroke-linecap="round">
+                <animate attributeName="stroke-dasharray" begin="0s" dur="200ms" values="0 ${Math.sqrt(width * width + height * height)};${Math.sqrt(width * width + height * height)} ${Math.sqrt(width * width + height * height)}" calcMode="linear" repeatCount="1" />
+            </line>
+        </svg>
+    `;
+
+    return svgCode;
+}
+
+
+function drawWinningLine(pattern) {
     if (pattern) {
         const [a, b, c] = pattern;
         const container = document.getElementById('board');
@@ -63,124 +144,3 @@ function drawWinLine(pattern) {
         container.insertBefore(svg, container.firstChild);
     }
 }
-
-
-// function handleCellClick(index) {
-//     // Überprüfen, ob das Feld bereits belegt ist
-//     if (fields[index] === null) {
-//         // Das Feld ist frei, wir setzen den Spielstein und den HTML-Code
-//         fields[index] = currentPlayer;
-//         const cell = document.getElementById(`cell-${index}`);
-//         cell.innerHTML = currentPlayer === 'circle' ? generateCircleSVGCode() : generateCrossSVGCode();
-        
-//         // Entfernen des onclick-Attributs, um weitere Klicks zu verhindern
-//         cell.removeAttribute('onclick');
-
-//         // Überprüfen, ob das Spiel vorbei ist
-//         const winPattern = checkWin();
-//         if (winPattern) {
-//             drawWinLine(winPattern);
-//         } else {
-//             // Wechseln zum nächsten Spieler
-//             currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
-//         }
-//     }
-// }
-
-
-function handleClick(cell, index) {
-    if (fields[index] === null) {
-        fields[index] = currentPlayer;
-        cell.innerHTML = currentPlayer === 'circle' ? generateCircleSVG() : generateCrossSVG();
-        cell.onclick = null;
-        currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
-        
-        if (isGameFinished()) {
-            const winCombination = getWinningCombination();
-            drawWinningLine(winCombination);
-        }
-    }
-}
-
-function isGameFinished() {
-    return fields.every((field) => field !== null) || getWinningCombination() !== null;
-}
-
-
-function getWinningCombination() {
-    for (let i = 0; i < WINNING_COMBINATIONS.length; i++) {
-        const [a, b, c] = WINNING_COMBINATIONS[i];
-        if (fields[a] === fields[b] && fields[b] === fields[c] && fields[a] !== null) {
-            return WINNING_COMBINATIONS[i];
-        }
-    }
-    return null;
-}
-
-
-function render() {
-    const container = document.getElementById('content');
-
-    // Erstellen des HTML-Codes für die Tabelle
-    //funktioniert noch nicht!!!
-    let tableHtml = '<table id="board">';
-    for (let i = 0; i < 3; i++) {
-        tableHtml += '<tr>';
-        for (let j = 0; j < 3; j++) {
-            const index = i * 3 + j;
-            tableHtml += `<td id="cell-${index}" class="${fields[index] || ''}"`;
-            if (fields[index] === null) {
-                tableHtml += ` onclick="handleClick(${index})"`;
-            }
-            tableHtml += '>';
-            if (fields[index] === 'circle') {
-                tableHtml += generateCircleSVGCode();
-            } else if (fields[index] === 'cross') {
-                tableHtml += generateCrossSVGCode();
-            }
-            tableHtml += '</td>';
-        }
-        tableHtml += '</tr>';
-    }
-    tableHtml += '</table>';
-
-    // Rendern der Tabelle im Container mit innerHTML
-    container.innerHTML = tableHtml;
-}
-
-
-function generateCircleSVGCode() {
-    const width = 70;
-    const height = 70;
-    const color = "#00B0EF";
-
-    const svgCode = `
-        <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="${width / 2}" cy="${height / 2}" r="${Math.min(width, height) / 2 - 5}" fill="none" stroke="${color}" stroke-width="10">
-                <animate attributeName="r" begin="0s" dur="200ms" values="0;${Math.min(width, height) / 2 - 5}" calcMode="linear" repeatCount="1" />
-            </circle>
-        </svg>
-    `;
-
-    return svgCode;
-}
-
-function generateCrossSVGCode() {
-    const width = 50;
-    const height = 50;
-    const color = "#FFC000";
-
-    const svgCode = `
-        <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
-            <line x1="0" y1="0" x2="${width}" y2="${height}" stroke="${color}" stroke-width="10" stroke-dasharray="${Math.sqrt(width * width + height * height)} 0" stroke-linecap="round">
-                <animate attributeName="stroke-dasharray" begin="0s" dur="200ms" values="0 ${Math.sqrt(width * width + height * height)};${Math.sqrt(width * width + height * height)} ${Math.sqrt(width * width + height * height)}" calcMode="linear" repeatCount="1" />
-            </line>
-            <line x1="0" y1="${height}" x2="${width}" y2="0" stroke="${color}" stroke-width="10" stroke-dasharray="${Math.sqrt(width * width + height * height)} 0" stroke-linecap="round">
-                <animate attributeName="stroke-dasharray" begin="0s" dur="200ms" values="0 ${Math.sqrt(width * width + height * height)};${Math.sqrt(width * width + height * height)} ${Math.sqrt(width * width + height * height)}" calcMode="linear" repeatCount="1" />
-            </line>
-        </svg>
-    `;
-
-    return svgCode;
-}
-
